363ce115d315e0695e33eae3a73019a08894314a
Luiz Henrique Almeida da Silva <luizhrqas@gmail.com>
2017-12-06 18:48:53 -0200

    fixed socket.io config for heroku

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
-const io = require('socket.io')(process.env.APP_PORT);
+const express = require('express');
 const {
   socketExchanges,
   socketRefresh,
 } = require('./sockets');
 const { log, } = require('./helpers');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+
+const server = express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const io = socketIO(server);
 
 const app = async () => {
+  let intervals = [];
+
   io.on('connection', (socket) => {
     console.log('exchange-server-backend: someone just connected!');
     socketExchanges(socket);
-    setInterval(() => socketRefresh(socket), 60000);
+    intervals[socket.id] = setInterval(() => socketRefresh(socket), 60000);
+
+    socket.on('disconnect', () => {
+      console.log('someone just disconnected!');
+      clearInterval(intervals[socket.id]);
+      intervals = intervals.filter((v, k) => k === socket.id);
+    })
   });
+
   log('exchange-server-backend: waiting for connections...');
 };
 
